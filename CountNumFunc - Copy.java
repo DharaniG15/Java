da64dd82e38e088699831a7936ea589f64c0792e
Dharani G <105649798+DharaniG15@users.noreply.github.com>
2024-11-05 09:24:57 +0530

    Delete CountNumFunc - Copy.java

--- CountNumFunc - Copy.java
+++
@@ -1,8 +0,0 @@
-class CountNumFunc{
-    public static int countLength(int num){
-        return String.valueOf(Math.abs(num)).length();
-    }
-    public static void main(String[] args) {
-        System.out.println(countLength(-6789));
-    }
-}
